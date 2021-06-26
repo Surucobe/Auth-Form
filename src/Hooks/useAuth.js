d748af2d291a0e2496b426a4bc567f98405fc84b
Surucobe <garudahorror25@gmail.com>
2021-06-25 20:51:03 -0400

    inicializacion de firebase y primeros pasos en la creacion del hook the autenticacion con firebase para el proyecto

---
+++ src/Hooks/useAuth.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+//Inicializar firebase
+firebase.initializeApp({
+	apiKey: process.env.REACT_APP_FRB_API_KEY,
+	authDomain: process.env.REACT_APP_FRB_DOMAIN,
+	projectId: process.env.REACT_APP_FRB_PROJECT,
+	storageBucket: process.env.REACT_APP_FRB_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FRB_SENDER,
+	appID: process.env.REACT_APP_FRB_APP
+});
+
+const AuthContext = createContext();
+
+//Hook para que los componentes hijos consigan el objeto auth
+//y re renderizen cuando este cambie
+
+export const useAuth = () =>{
+	return useContext(AuthContext);
+}
+
+//Provider hook que creara el el objeto auth y manejara el state
+export const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+}
