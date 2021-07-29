0e166407e2102d5fea24ac5ff268823078d7735e
Surucobe <garudahorror25@gmail.com>
2021-07-28 21:24:48 -0400

    Project is finish, debugging starts tomorrow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LoginForm from './components/auth/LoginForm';
 import ConfirmForm from './components/auth/ConfirmForm';
+import PrivateRoute from './components/Routes/PrivateRoute';
+import Dashboard from './components/Dashboard/Dashboard';
+import NotFound from './components/NotFound/NotFound';
 
 import Layout from './components/Layout/Layout';
 
 const App = () => {
   return(
     <Router>
-    	<Switch>
-    		<Layout>
+    	<Layout>
+    		<Switch>
+    			<PrivateRoute exact path='/'>
+    				<Dashboard />
+    			</PrivateRoute>
     			<Route path='/'>
     				<LoginForm />
     			</Route>
     			<Route path='/'>
     				<ConfirmForm />
     			</Route>
-    		</Layout>
-    	</Switch>
+    			<Route component={NotFound} />
+    		</Switch>
+    	</Layout>
     </Router>
     )
 }
